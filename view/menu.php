303652ad8d1ffae75716bfe88fb3392c7614277a
vpmaciel <vpmaciel@gmail.com>
2020-10-23 11:12:47 -0300

    --- Auto-Git Commit ---

--- view/menu.php
+++ view/menu.php
@@ -3,28 +3,26 @@
    <li><a href="index.php">Home</a></li>
    <li class='active'><a href='#'>Sistema</a>
       <ul>         
-         <li><a href='usuario.php'>Cadastro de Usuário</a></li>         
-         <li><a href='login.php'>Login</a></li>
-         <li><a href='#'>Logout</a></li>
-         <li><a href='#'>Candidato</a>
-            <ul>
-               <li><a href="pessoa.php">Dados Pessoais</a></li>
-               <li><a href="curso.php">Cursos</a></li>
-               <li><a href="certificado.php">Certificados</a></li>               
-               <li><a href="experienciaprofissional.php">Experiência Profissional</a></li>
-               <li><a href="objetivoprofissional.php">Objetivo Profissional</a></li>
-               <li><a href="idioma.php">Idiomas</a></li>
-               <li><a href="habilidade.php">Habilidades</a></li>
-               <li><a href="vagas.php">Vagas</a></li>
-               <li><a href="curriculo.php">Exibir Currículo</a></li>
-            </ul>
-         </li>
-         <li><a href='#'>Empresa</a>
-            <ul>
-               <li><a href="candidatovagalista.php">Selecionar Candidatos</a></li>
-               <li><a href="publicarvaga.php">Publicar Vaga</a></li>               
-            </ul>
-         </li>
+         <li><a href='usuario.php'>Novo Usuário</a></li>         
+         <li><a href='login.php'>Entrar</a></li>
+         <li><a href='#'>Sair</a></li>
+      </ul>
+      <li class='active'><a href='#'>Candidato</a>
+      <ul>                  
+         <li><a href="pessoa.php">Dados Pessoais</a></li>
+         <li><a href="curso.php">Cursos</a></li>
+         <li><a href="certificado.php">Certificados</a></li>               
+         <li><a href="experienciaprofissional.php">Experiência Profissional</a></li>
+         <li><a href="objetivoprofissional.php">Objetivo Profissional</a></li>
+         <li><a href="idioma.php">Idiomas</a></li>
+         <li><a href="habilidade.php">Habilidades</a></li>
+         <li><a href="vagas.php">Vagas</a></li>
+         <li><a href="curriculo.php">Exibir Currículo</a></li>
+      </ul>
+      <li class='active'><a href='#'>Empresa</a>
+      <ul>         
+         <li><a href="candidatovagalista.php">Selecionar Candidatos</a></li>
+         <li><a href="publicarvaga.php">Publicar Vaga</a></li>                       
       </ul>
    <li><a href='#'>Sobre</a></li>
    <li><a href='#'>Contato</a></li>
