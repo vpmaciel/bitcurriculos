2425475c06016478a8e07ef27aa1fefd98d43010
vpmaciel <vpmaciel@gmail.com>
2020-11-04 04:53:06 -0300

    --- Auto-Git Commit ---

--- view/menu.php
+++ view/menu.php
@@ -1,28 +1,18 @@
-<div id='menu' align="center">
-<table align="center">
-   <tr>
-   <td><a href="index.php">Home</a></td>
-   <td><a href='usuario.php'>Registrar</a></td>
-   <td><a href='login.php'>Entrar</a></td>
-   <td><a href='#'>Sair</a></td>
-   </tr>
-   <tr>
-   <td><a href="pessoa.php">Dados Pessoais</a></td>
-   <td><a href="curso.php">Cursos</a></td>
-   <td><a href="certificado.php">Certificados</a></td>
-   <td><a href="experienciaprofissional.php">Experiência Profissional</a></td>
-   </tr>
-   <tr>
-   <td><a href="objetivoprofissional.php">Objetivo Profissional</a></td>
-   <td><a href="idioma.php">Idiomas</a></td>
-   <td><a href="habilidade.php">Habilidades</a></td>
-   <td><a href="vagas.php">Vagas</a></td>
-   </tr>
-   <tr>
-   <td><a href="curriculo.php">Exibir Currículo</a></td>
-   <td><a href="candidatovagalista.php">Selecionar Candidatos</a></td>
-   <td><a href="publicarvaga.php">Publicar Vaga</a></td>    
-   <td><a href='#'>Contato</a></td>
-   </tr>
-</table>   
+<div class="menu">
+   <a href="index.php">Home</a><br>
+   <a href='usuario.php'>Registrar</a><br>
+   <a href='login.php'>Entrar</a><br>
+   <a href='#'>Sair</a><br>
+   <a href="pessoa.php">Dados Pessoais</a><br>
+   <a href="curso.php">Cursos</a><br>
+   <a href="certificado.php">Certificados</a><br>
+   <a href="experienciaprofissional.php">Experiência Profissional</a><br>
+   <a href="objetivoprofissional.php">Objetivo Profissional</a><br>
+   <a href="idioma.php">Idiomas</a><br>
+   <a href="habilidade.php">Habilidades</a><br>
+   <a href="vagas.php">Vagas</a><br>
+   <a href="curriculo.php">Exibir Currículo</a><br>
+   <a href="candidatovagalista.php">Selecionar Candidatos</a><br>
+   <a href="publicarvaga.php">Publicar Vaga</a><br>    
+   <a href='#'>Contato</a>
 </div>
