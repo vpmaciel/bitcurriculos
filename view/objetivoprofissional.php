b3fb06dee6f381c89d3bc345aec64b3a4c3c2e80
vpmaciel <vpmaciel@gmail.com>
2020-11-22 18:23:24 -0300

    22-11-2020

--- view/objetivoprofissional.php
+++ view/objetivoprofissional.php
@@ -18,13 +18,13 @@ echo $FORM;
 
 echo $TABLE;
 
-echo $TR . $TD . $LABEL . 'Cargo' . $LABEL_ . $TD_; 
+echo $TR . $TD . $LABEL . 'Cargo' . $LABEL_ . $TD_ . $TR_;
 $pessoa['nome'] = isset($_POST['nome'])?$_POST['nome']:'';
 $CARGO = '<input type="text" name="nome" required size="70" minlength="10" maxlength="50" value="' . $pessoa['nome'] .'">';
-echo $TD . $CARGO . $TD_ . $TR_;
+echo $TR. $TD . $CARGO . $TD_ . $TR_;
 
-echo $TR . $TD . $LABEL . 'Pretensão salarial' . $LABEL_ . $TD_; 
-echo $TD;
+echo $TR . $TD . $LABEL . 'Pretensão salarial' . $LABEL_ . $TD_ . $TR_;
+echo $TR. $TD;
 $SELECT = '<select name="$pretensao_salario">';
 echo $SELECT;
 foreach ($pretensao_salario_lista as $indice => $pretensao_salario) {	
@@ -35,8 +35,8 @@ foreach ($pretensao_salario_lista as $indice => $pretensao_salario) {
 }
 echo $SELECT_ . $TD_ . $TR_;
 
-echo $TR . $TD . $LABEL . 'Contrato' . $LABEL_ . $TD_; 
-echo $TD;
+echo $TR . $TD . $LABEL . 'Contrato' . $LABEL_ . $TD_ . $TR_;
+echo $TR. $TD;
 $SELECT = '<select name="contrato">';
 echo $SELECT;
 foreach ($contrato_lista as $indice => $contrato) {	
@@ -47,10 +47,10 @@ foreach ($contrato_lista as $indice => $contrato) {
 }
 echo $SELECT_ . $TD_ . $TR_;
 
-echo $TR . $TD . $LABEL . '&nbsp;' . $LABEL_ . $TD_; 
+echo $TR . $TD . $LABEL . '&nbsp;' . $LABEL_ . $TD_ . $TR_;
 
 $SUBMIT = '<input type="submit" value="Salvar" onclick=\'return confirmar();\'>';
-echo $TD . $SUBMIT . $TD_ . $TR_;
+echo $TR. $TD . $SUBMIT . $TD_ . $TR_;
 
 echo $TABLE_;
 
