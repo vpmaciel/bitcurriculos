f4fc23c83bd99a1c69ca72909d9127c38774866b
vpmaciel <vpmaciel@gmail.com>
2020-11-29 22:28:22 -0300

    --- Auto-Git Commit ---

---
+++ lib/enviar_email.php
@@ -0,0 +1,9 @@
+<?php
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+$opcoes = $_POST['escolhas'];
+$mensagem = $_POST['msg'];
+$data_envio = date('d/m/Y');
+$hora_envio = date('H:i:s');
