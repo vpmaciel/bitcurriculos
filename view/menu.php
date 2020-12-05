a9b79ab6310e6775c6932379215617ee70465ffd
vpmaciel <vpmaciel@gmail.com>
2020-12-05 09:13:49 -0300

    --- Auto-Git Commit ---

--- view/menu.php
+++ view/menu.php
@@ -7,17 +7,19 @@
       <a href="pessoa.php">Dados Pessoais</a><br>
       <a href="curso.php">Cursos</a><br>
       <a href="certificacao.php">Certificação</a><br>
-      <a href="experienciaprofissional.php">Experiência Profissional</a><br>
-      <a href="objetivoprofissional.php">Objetivo Profissional</a><br>
+      <a href="experiencia_profissional.php">Experiência Profissional</a><br>
+      <a href="objetivo_profissional.php">Objetivo Profissional</a><br>
       <a href="idioma.php">Idiomas</a><br>
       <a href="habilidade.php">Habilidades</a><br>
       <a href="vagas.php">Pesquisar Vagas</a><br>
       <a href="curriculo.php">Exibir Currículo</a><br>
       <a href="curriculo.php">Testes de Personalidade</a><br>-<br>
-      <a href="publicarvaga.php">Publicar Vaga de Emprego</a><br>-<br>
+      <a href="publicar_vaga.php">Publicar Vaga de Emprego</a><br>-<br>
       <a href="servico.php">Serviços</a><br>
       <a href='prova.php'>Provas</a><br>
       <a href="apostila.php">Apostilas</a><br>
       <a href="livro.php">Livros</a><br>
       <a href="treinamento.php">Treinamentos</a><br>
+      <a href="noticias.php">Notícias</a><br>
+
    </div>
