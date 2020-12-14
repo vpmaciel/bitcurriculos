cda154b7dbf3c6db24221c6851d2157337b8895f
vpmaciel <vpmaciel@gmail.com>
2020-12-14 06:57:09 -0300

    --- Auto-Git Commit ---

--- view/menu.php
+++ view/menu.php
@@ -1,19 +1,20 @@
-<div class="linha">
-   <div class="coluna esquerda">
-      <a href="index.php">Home</a><br>
-      <a href="registro.php">Registrar</a><br>
-      <a href="login.php">Entrar</a><br>
-      <a href="logoff.php">Sair</a><br>-<br>
-      <a href="pessoa.php">Dados Pessoais</a><br>
-      <a href="curso.php">Cursos</a><br>
-      <a href="certificacao.php">Certificação</a><br>
-      <a href="experiencia_profissional.php">Experiência Profissional</a><br>
-      <a href="objetivo_profissional.php">Objetivo Profissional</a><br>
-      <a href="idioma.php">Idiomas</a><br>
-      <a href="habilidade.php">Habilidades</a><br>
-      <a href="vagas.php">Pesquisar Vagas</a><br>
-      <a href="curriculo.php">Exibir Currículo</a><br>-<br>
-      <a href="publicar_vaga.php">Publicar Vaga de Emprego</a><br>-<br>
-      <a href="servicos.php">Cadastrar Empresas e Serviços</a><br>
-      <a href="servicos_pesquisa.php">Pesquisar Empresas e Serviços</a><br>
-   </div>
+<?php
+echo '<div class="linha">';
+echo '<div class="coluna esquerda">';
+echo '<a href="index.php">Home</a><br>';
+echo '<a href="registro.php">Registrar</a><br>';
+echo '<a href="login.php">Entrar</a><br>';
+echo '<a href="logoff.php">Sair</a><br>-<br>';
+echo '<a href="pessoa.php">Dados Pessoais</a><br>';
+echo '<a href="curso.php">Cursos</a><br>';
+echo '<a href="certificacao.php">Certificação</a><br>';
+echo '<a href="experiencia_profissional.php">Experiência Profissional</a><br>';
+echo '<a href="objetivo_profissional.php">Objetivo Profissional</a><br>';
+echo '<a href="idioma.php">Idiomas</a><br>';
+echo '<a href="habilidade.php">Habilidades</a><br>';
+echo '<a href="vagas.php">Pesquisar Vagas</a><br>';
+echo '<a href="curriculo.php">Exibir Currículo</a><br>-<br>';
+echo '<a href="publicar_vaga.php">Publicar Vaga de Emprego</a><br>-<br>';
+echo '<a href="servicos.php">Cadastrar Empresas e Serviços</a><br>';
+echo '<a href="servicos_pesquisa.php">Pesquisar Empresas e Serviços</a><br>';
+echo '</div>';
