5d8ded6cde8e05f2f7de244b61703ca1add9a853
vpmaciel <vpmaciel@gmail.com>
2021-01-25 00:41:55 -0300

    --- Auto-Git Commit ---

--- view/certificacao.js
+++ view/certificacao.js
@@ -6,22 +6,20 @@ var botao_salvar = document.getElementById("btn_salvar");
 
 botao_salvar.addEventListener("click", function(event) {
     event.preventDefault();    
-    if (isNaN(cer_ano_obtencao.value)){
-        document.getElementById('msg_cer_ano_obtencao').textContent = ' Digite um Ano de Obtenção de 1900 a 3000 !';
-        return;
-    }
-    if (cer_ano_obtencao.value < 1900 || cer_ano_obtencao.value > 3000) {
-        document.getElementById('msg_cer_ano_obtencao').textContent = ' Digite um Ano de Obtenção de 1900 a 3000 !';
-        return;
+    var campos_validos = true;
+    if (isNaN(cer_ano_obtencao.value) || cer_ano_obtencao.value < 1900 || cer_ano_obtencao.value > 3000) {
+        document.getElementById('msg_cer_ano_obtencao').textContent = ' * O campo Ano de obtenção é obrigatório e deve estar entre de 1900 a 3000 !';
+        campos_validos = false;
     } 
     alert(cer_certificacao.value.length);
-    if (cer_certificacao.value.length > 50){
-        alert("oi");
-        document.getElementById('msg_cer_certificacao').textContent = ' O campo Certificação não pode ter mais que 50 caracteres !';
-        return;
+    if (cer_certificacao.value.length == 0 || cer_certificacao.value.length > 50){        
+        document.getElementById('msg_cer_certificacao').textContent = ' * O campo Certificação é obrigatório e não pode ter mais que 50 caracteres !';
+        campos_validos = false;
     }
-    if (cer_instituicao.value.length > 50){
-        document.getElementById('msg_cer_instituicao').textContent = ' O campo Instituição não pode ter mais que 50 caracteres !';
-        return;
+    if (cer_instituicao.value.length == 0 || cer_instituicao.value.length > 50){
+        document.getElementById('msg_cer_instituicao').textContent = ' * O campo Instituição é obrigatório e não pode ter mais que 50 caracteres !';
+        campos_validos = false;
     }
+
+    return campos_validos;
 });
