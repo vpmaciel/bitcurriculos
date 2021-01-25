b5ce86032a9d5e25261c1eb8193df639ff42de77
vpmaciel <vpmaciel@gmail.com>
2021-01-25 11:01:30 -0300

    --- Auto-Git Commit ---

--- view/certificacao.js
+++ view/certificacao.js
@@ -3,12 +3,12 @@ var cer_certificacao = document.getElementById('cer_certificacao');
 var cer_instituicao = document.getElementById('cer_instituicao');
 var cer_ano_obtencao = document.getElementById('cer_ano_obtencao');
 var botao_salvar = document.getElementById("btn_salvar");
-botao_salvar.addEventListener("click", function(event) {
+botao_salvar.addEventListener('click', function(event) {
     event.preventDefault();    
     var campos_validos = true;
     if (isNaN(cer_ano_obtencao.value) || cer_ano_obtencao.value < 1900 || cer_ano_obtencao.value > 3000) {
         document.getElementById('msg_cer_ano_obtencao').textContent = ' * O campo Ano de obtenção é obrigatório e deve estar entre de 1900 a 3000 !';
-        campos_validos = false;
+        return;
     }     
     if (cer_certificacao.value.length == 0 || cer_certificacao.value.length > 50){        
         document.getElementById('msg_cer_certificacao').textContent = ' * O campo Certificação é obrigatório e não pode ter mais que 50 caracteres !';
